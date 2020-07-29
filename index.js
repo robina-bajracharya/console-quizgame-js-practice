e72cd13a47d06797229ddd4ca60204e976929914
brobina <bajracharya.robina@gmail.com>
2020-07-29 17:29:41 +0545

    Second part completed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 (function(){
+	var random,userAnswer;
 	var Question = function(question,answers,correctAnswer){
 	this.question = question;
 	this.answers = answers;
@@ -10,12 +11,21 @@
 		console.log('0 '+this.answers[0]);
 		console.log('1 '+this.answers[1]);
 	}
-	Question.prototype.checkAnswer = function(){
+	Question.prototype.checkAnswer = function(keepScore){
+		var sc;
 		if (userAnswer == this.correctAnswer){
+
 			console.log("Your answer is correct!");
+			sc =keepScore(true);
 		}else{
 			console.log("Your answer is incorrect!");
+			sc = keepScore(false);
 		}
+		this.displayScore(sc);
+	}
+	Question.prototype.displayScore = function(sc){
+		console.log("Your final Score is : "+sc);
+
 	}
 	var ques1 = new Question("Is javascript the coolest programming language?",['yes','no'],0);
 	var ques2 = new Question("What is the name of the teacher?",['jonas','john'],0);
@@ -23,8 +33,28 @@
 	var questions = [];
 	questions.push(ques1);
 	questions.push(ques2);
-	var random = Math.floor(Math.random()*questions.length);
+	function scoreCalculation(){
+		var score = 0;
+		return function(correct){
+			if (correct){
+			score++;
+		}
+		return score;
+		}
+	}
+	var keepScore = scoreCalculation();
+	function nextQuestion(){
+	random = Math.floor(Math.random()*questions.length);
 	questions[random].displayQuestions();
-	var userAnswer = prompt("Enter your answer in numbers:");
-	questions[random].checkAnswer();
+	userAnswer = prompt("Enter your answer in numbers:");
+	
+	if (userAnswer !== 'exit'){
+		questions[random].checkAnswer(keepScore);
+		nextQuestion();
+
+	}
+	
+	}
+	
+	nextQuestion();
 })();
